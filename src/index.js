58c73a191b50707647b3597b57ca7701729b2990
Crelia <jefferson.crelia@gmail.com>
2020-11-21 11:31:59 -0600

    working on ui

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 //Jefferson Crelia
 //javascript index
+
+var btn = document.createElement("btn");
+btn.innerHTML = "Add Item";
+
+var sidenav = document.getElementsByTagName("sidenav")[0];
+sidenav.appendChild(btn);
+
+btn.addEventListener("click", function() { alert("adding item");});
