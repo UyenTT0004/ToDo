6f278a6a09dec9f381539e5931886fa8dc54cb33
Crelia <jefferson.crelia@gmail.com>
2020-11-21 18:14:34 -0600

    Add list elements with the option to remove, and check/uncheck

--- src/app.js
+++ src/app.js
@@ -1,7 +1,61 @@
 'use strict'
 
-function addTask() {
-	document.getElementById("addTask").stly.display = 'block';
+//create close button, add to each item
+var myList = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myList.length; i++) {
+	var span = document.createElement("SPAN");
+	var txt = document.createTextNode("\u00D7");
+	span.className = "close";
+	span.appendChild(txt);
+	myList[i].appendChild(span);
 }
 
-document.getElementById('textboxid').style.height="200px";
+//clicked close button hides list items
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+	close[i].onclick = function() {
+		var div = this.parentElement;
+		div.style.display = "none";
+	}
+}
+
+
+//add checked symbol when clicked
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+	if(ev.target.tagName == 'LI') {
+		ev.target.classList.toggle('checked');
+	}
+}, false);
+
+
+//create new item when clicking add
+function newTask() {
+	var li = document.createElement("li");
+	var inVal = document.getElementById("myInput").value;
+	var t = document.createTextNode(inVal);
+	li.appendChild(t);
+	if(inVal === '') {
+		alert("no input given");
+	} else {
+		document.getElementById("myUL").appendChild(li);
+	}
+
+	document.getElementById("myInput").value = "";
+
+	var span = document.createElement("SPAN");
+	var txt = document.createTextNode("\u00D7");
+	span.className = "close";
+	span.appendChild(txt);
+	li.apendChild(span);
+
+	for(i = 0; i < close.length; i++) {
+		close[i].onclick = function() {
+			var div = this.parentElement;
+			div.style.display = "none";
+		}
+	}
+}
+
